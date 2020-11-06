f926097afeea8dad43cf9d3fdf7fc26b21a9b273
burhan122 <44556135+burhan122@users.noreply.github.com>
2020-11-07 00:16:22 +0600

    Update webhook.js

--- commands/webhook.js
+++ commands/webhook.js
@@ -1,1 +1,14 @@
 
+var check = {
+  notiFy() {
+    var url = {
+      command: "get_notify",
+      content: "text",
+      user_id: user.id,
+      redirect_to: "http://t.me/" + bot.name
+    }
+    return Libs.Webhooks.getUrlFor(url)
+  }
+}
+
+Bot.sendMessage("[" + check.notiFy() + "]")
