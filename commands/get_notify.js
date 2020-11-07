95b964dc14afd389af7f949295f40fa4916c38d8
burhan122 <44556135+burhan122@users.noreply.github.com>
2020-11-07 22:39:04 +0600

    Update get_notify.js

--- commands/get_notify.js
+++ commands/get_notify.js
@@ -1,14 +1,15 @@
 
 var api = Libs.SellerCoinbase.api()
-var sing = Libs.SellerCoinbase.sing()
+var sing = Libs.SellerCoinbase.sing({ method: "GET", path: "notifications" })
+var timeStamp = Libs.SellerCoinbase.timeStamp()
 HTTP.get({
-  "url": "https://api.coinbase.com/v2/notifications",
-  "success": "onSuccesResult",
-  "error": "error",
-  "headers": {
-    "CB-ACCESS-KEY": api.APIKey ,
-    "CB-ACCESS-SIGN":sing,
-    "CB-ACCESS-TIMESTAMP": Libs.SellerCoinbase.timeStamp(),
+  url: "https://api.coinbase.com/v2/notifications",
+  success: "onSuccesResult",
+  error: "error",
+  headers: {
+    "CB-ACCESS-KEY": api.APIKey,
+    "CB-ACCESS-SIGN": sing,
+    "CB-ACCESS-TIMESTAMP":timeStamp ,
     "CB-VERSION": "2019-11-15"
   }
 })
