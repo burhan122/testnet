bce7cd1418d16dd1a0e65b05664508af36af439f
burhan122 <44556135+burhan122@users.noreply.github.com>
2020-11-08 00:48:15 +0600

    Create Button.js

---
+++ libs/Button.js
@@ -0,0 +1,30 @@
+function GetButton(){
+return Bot.getProperty("button", {
+  balance: "💰 {balanceid} {cur}",
+  deposit: "📥 Deposits",
+  reinvest: "♻️ Reinvest",
+  payment: "📤 Withdraws",
+  status: "📉 Status",
+  referral: "🏄 Referrals",
+  wallet: "💼 Wallets",
+  calco: "🖥 Calculateds",
+  support: "📞 Supports",
+  abouts: "‼️ Abouts",
+  livepay: "⚜ Live Pays",
+  faq: "📰 Faqs",
+  investmenthist: "📉 My Investments",
+  withrowalshist: "📈 My Withdrawals",
+  accounthist: "🗂 Historys",
+  accounthistb: "🗂 Historys",
+  walletb: "💼 Wallets",
+  bonus: "🎁 My Bonus",
+  back: "⬅️ Returns"
+})
+}
+
+function ReturnButton(){
+
+}
+prublic({
+
+})
