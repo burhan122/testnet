d0c586992923665973865a464611157a79a329aa
burhan122 <44556135+burhan122@users.noreply.github.com>
2020-11-08 01:15:48 +0600

    Update Button.js

--- libs/Button.js
+++ libs/Button.js
@@ -22,9 +22,72 @@ return Bot.getProperty("button", {
 })
 }
 
+
+
+
 function ReturnButton(){
-
+var type = message;
+var buttonOn = GetButton()
+  
+  
+  
+  if(type==buttonOn.balance.slice(0,1)){
+   return 
+   }
+if(type==buttonOn.deposit.slice(0,1)){
+   return 
+   }  
+  if(type==buttonOn.reinvest.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.payment.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.status.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.referral.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.wallet.slice(0,1)||type==buttonOn.walletb){
+   return 
+   }
+  if(type==buttonOn.calco.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.support.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.abouts.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.livepay.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.faq.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.investmenthist.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.withrowalshist.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.accounthist.slice(0,1)||type==buttonOn.accounthistb.slice(0,1)){
+   return 
+   }
+  if(type==buttonOn.bonus){
+   return 
+   }
+if(type==buttonOn.back){
+   return 
+   }
+  
 }
-prublic({
 
+
+publish({
+ 
+  
 })
+
