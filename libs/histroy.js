3eeb4791b3c4d4804acd691f57d7fc59c93dc04e
burhan122 <44556135+burhan122@users.noreply.github.com>
2020-12-09 19:01:58 +0600

    Update histroy.js

--- libs/histroy.js
+++ libs/histroy.js
@@ -1,1 +1,66 @@
 
+function setupHistry(name, value, total) {
+  function list(options) {
+    var result = Bot.getProperty(options, [])
+    return result
+  }
+
+  var result1 = list(name)
+
+  if (result1.length == total) {
+    delete result1[0]
+    Bot.setProperty(
+      name,
+      result1.filter(function(x) {
+        return x != null
+      }),
+      "json"
+    )
+  }
+
+  var result2 = list(name)
+
+  result2.push(value)
+  Bot.setProperty(name, result2, "json")
+}
+
+var setup = {
+  hist: setupHistry
+}
+
+var json = {
+  name: 100
+}
+
+setup.hist("list" + user.telegramid, json, 5)
+
+
+
+
+function list(options) {
+  var result = Bot.getProperty(options, [])
+  return result
+}
+
+
+var msg =
+  "*🗂 Transactions History*\n_Here you can find the last 15 transactions related to your account_\n\n"
+
+
+if (hist.length > 15) {
+  var max = hist.length * 1
+  var min = max - 15
+  var history = hist.slice(min, max)
+} else {
+var history = list("list" + user.telegramid)
+
+if (history.length > 0) {
+  for (var i in history) {
+    var data = history[i]
+
+    msg += data.name+"\n"
+  }
+}
+Bot.sendMessage(msg, { disable_web_page_preview: true })
+  
+  public
